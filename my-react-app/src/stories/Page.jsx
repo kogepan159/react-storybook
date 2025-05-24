46285e85515b1f136bdc5fd610f82c1b652c6365
堅固潤也 <kogepan6852@yahoo.co.jp>
2025-05-24 16:46:09 +0900

    chromatic追加対応

--- my-react-app/src/stories/Page.jsx
+++ my-react-app/src/stories/Page.jsx
@@ -25,9 +25,7 @@ export const Page = () => {
           process starting with atomic components and ending with pages.
         </p>
         <p>
-          Render pages with mock data. This makes it easy to build and review page states without
-          needing to navigate to them in your app. Here are some handy patterns for managing page
-          data in Storybook:
+          あああああいいいううう
         </p>
         <ul>
           <li>
